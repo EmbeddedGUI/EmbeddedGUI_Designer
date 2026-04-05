ce34f01cc7e4631ecd1d02c56d98728c9dec199f
wenbo <wenbo13579@163.com>
2026-04-06 07:57:40 +0800

    style: compact page tab rail

--- ui_designer/tests/ui/test_main_window_file_flow.py
+++ ui_designer/tests/ui/test_main_window_file_flow.py
@@ -6327,8 +6327,8 @@ class TestMainWindowFileFlow:
         assert window.page_tab_bar.accessibleName() == (
             "Page tabs: 1 open page. Current page: main_page. Startup page: main_page. No dirty pages."
         )
-        assert window.page_tab_bar.height() == 36
-        assert window.page_tab_bar.tabMaximumWidth() == 168
+        assert window.page_tab_bar.height() == 34
+        assert window.page_tab_bar.tabMaximumWidth() == 156
         assert window.page_tab_bar.toolTip() == window.page_tab_bar.accessibleName()
 
         window._undo_manager.get_stack("main_page").push("<Page dirty='main' />")
