de5c6fcdcf26d3884cf24960831f103ff1a36075
wenbo <wenbo13579@163.com>
2026-04-28 13:36:12 +0800

    1. 更新egui配置

--- examples/HelloSimpleDemo/build.mk
+++ examples/HelloSimpleDemo/build.mk
@@ -1,11 +1,3 @@
-# Build configuration for HelloSimpleDemo
+# User build overrides for HelloSimpleDemo
 
-EGUI_CODE_SRC		+= $(EGUI_APP_PATH)
-EGUI_CODE_SRC		+= $(EGUI_APP_PATH)/resource
-EGUI_CODE_SRC		+= $(EGUI_APP_PATH)/resource/img
-EGUI_CODE_SRC		+= $(EGUI_APP_PATH)/resource/font
-
-EGUI_CODE_INCLUDE	+= $(EGUI_APP_PATH)
-EGUI_CODE_INCLUDE	+= $(EGUI_APP_PATH)/resource
-EGUI_CODE_INCLUDE	+= $(EGUI_APP_PATH)/resource/img
-EGUI_CODE_INCLUDE	+= $(EGUI_APP_PATH)/resource/font
+include $(EGUI_APP_PATH)/.designer/build_designer.mk
